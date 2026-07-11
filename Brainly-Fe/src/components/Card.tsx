692ef496dd1d038492efeef2472e9956d0144a9c
Raamanjal <gangwarraamanjal@gmail.com>
2026-07-12 04:57:20 +0530

    fix(content): render backend content types

--- Brainly-Fe/src/components/Card.tsx
+++ Brainly-Fe/src/components/Card.tsx
@@ -3,7 +3,7 @@ import { ShareIcon } from "../icons/ShareIcon";
 interface CardProps {
     title: string;
     link: string;
-    type: "twitter" | "youtube" | "image";
+    type: "tweet" | "video" | "image";
 }
 
 export function Card({title, link, type}: CardProps) {
@@ -29,9 +29,9 @@ export function Card({title, link, type}: CardProps) {
             </div>
 
             <div className="pt-4">
-                {type === "youtube" && <iframe className="w-full" src={link.replace("watch", "embed").replace("?v=", "/")} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>}
+                {type === "video" && <iframe className="w-full" src={link.replace("watch", "embed").replace("?v=", "/")} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>}
 
-                {type === "twitter" && <blockquote className="twitter-tweet">
+                {type === "tweet" && <blockquote className="twitter-tweet">
                     <a href={link.replace("x.com", "twitter.com")}></a> 
                 </blockquote>}
                 {type === "image" && <img className="w-full" src={link} alt={title} />}
