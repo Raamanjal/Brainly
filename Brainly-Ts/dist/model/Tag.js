19e28ebd06c262396a91efff6b6e48ed10bad1ce
Raamanjal <gangwarraamanjal@gmail.com>
2026-07-16 13:19:12 +0530

    feat: compile user scoped tag model

--- Brainly-Ts/dist/model/Tag.js
+++ Brainly-Ts/dist/model/Tag.js
@@ -1,6 +1,8 @@
-import { Schema, model } from "mongoose";
+import { Schema, model, Types } from "mongoose";
 const TagSchema = new Schema({
-    title: { type: String, required: true, unique: true }
+    title: { type: String, required: true, trim: true },
+    userId: { type: Types.ObjectId, ref: "User", required: true },
 });
+TagSchema.index({ userId: 1, title: 1 }, { unique: true });
 export const Tag = model("Tag", TagSchema);
 //# sourceMappingURL=Tag.js.map
