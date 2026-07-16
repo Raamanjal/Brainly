dd8416f5c92a0a22f62a00b9a401fe81fefdfa75
Raamanjal <gangwarraamanjal@gmail.com>
2026-07-16 13:19:06 +0530

    feat: register tag API routes

--- Brainly-Ts/src/index.ts
+++ Brainly-Ts/src/index.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import  authRouter from "./routes/authRouter.js";
 import contentRouter from "./routes/contentRouter.js";
 import  shareRouter  from "./routes/shareRouter.js";
+import tagRouter from "./routes/tagRouter.js";
 import { dbConnect } from "./utils/db.js";
 
 const app=express();
@@ -14,6 +15,7 @@ const PORT = process.env.PORT || 4000;
 app.use("/api/v1/auth",authRouter);
 app.use("/api/v1/content",contentRouter);
 app.use("/api/v1/brain",shareRouter);
+app.use("/api/v1/tags", tagRouter);
 
 
 dbConnect().then(()=>{
